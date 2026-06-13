72389153ac543b1fd4a790fa820d919351691408
dinhphuong257 <kimdinhphuong.vn@gmail.com>
2026-06-13 12:21:51 +0700

    style: make language switcher divider line thicker and indigo colored

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -207,7 +207,7 @@ export default function Sidebar({ isOpen, onClose }: SidebarProps) {
             </button>
             
             {/* Horizontal Divider Line */}
-            <div className="h-px bg-slate-400 dark:bg-slate-600 my-1 mx-2" />
+            <div className="h-[2px] bg-indigo-600 dark:bg-indigo-400 my-1.5 mx-3 rounded-full" />
 
             <button
               onClick={() => setLanguage("en")}
